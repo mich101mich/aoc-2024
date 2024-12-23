6de92a85048eedda86b1435e7616360b50f6e86e
mhillmann <mhillmann@uos.de>
2024-12-23 14:23:26 +0100

    Optimized day 23

--- src/days/day_23.rs
+++ src/days/day_23.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 use crate::utils::*;
 
 #[allow(unused)]
@@ -10,47 +12,72 @@ pub fn run() {
         .lines()
         .map(|l| sscanf!(l, "{str}-{str}").unwrap())
         .for_each(|(a, b)| {
-            conn.entry(a).or_insert_with(HashSet::new).insert(b);
-            conn.entry(b).or_insert_with(HashSet::new).insert(a);
+            conn.entry(a).or_insert_with(Vec::new).push(b);
+            conn.entry(b).or_insert_with(Vec::new).push(a);
         });
 
+    conn.iter_mut().for_each(|(_, v)| v.sort_unstable());
+
     fn recurse<'a>(
-        current: &mut HashSet<&'a str>,
-        conn: &HashMap<&'a str, HashSet<&'a str>>,
+        current: &mut String,
+        conn: &HashMap<&'a str, Vec<&'a str>>,
+        candidates: &[&'a str],
         out: &mut String,
-        visited: &mut HashSet<String>,
     ) {
-        let mut name = current.iter().copied().to_vec();
-        name.sort();
-        let name = name.join(",");
-        if !visited.insert(name.clone()) {
-            return;
-        }
-
-        let candidates = conn
-            .iter()
-            .filter(|(k, v)| !current.contains(*k) && current.is_subset(v))
-            .map(|(k, _)| *k)
-            .to_vec();
-
         if candidates.is_empty() {
-            if name.len() > out.len() {
-                *out = name;
+            if current.len() > out.len() {
+                *out = current.clone();
             }
             return;
         }
 
-        for c in candidates {
-            current.insert(c);
-            recurse(current, conn, out, visited);
-            current.remove(c);
+        let start_len = current.len();
+
+        let candidates = &mut &candidates[..]; // mut ref to a slice allows "removing" elements from the front
+        let mut next_candidates = Vec::with_capacity(candidates.len());
+        while let Some((&c, rest)) = candidates.split_first() {
+            *candidates = rest;
+            // The one part of the C++ std lib that is better than Rust's: Sorted list operations
+            let mut iter = conn[c].iter().peekable();
+            next_candidates.clear();
+            next_candidates.extend(candidates.iter().copied().filter(|x| {
+                while let Some(&y) = iter.peek() {
+                    match x.cmp(y) {
+                        std::cmp::Ordering::Less => return false,
+                        std::cmp::Ordering::Equal => {
+                            iter.next();
+                            return true;
+                        }
+                        std::cmp::Ordering::Greater => {
+                            iter.next();
+                        }
+                    }
+                }
+                false
+            }));
+            current.push(',');
+            current.push_str(c);
+            recurse(current, conn, &next_candidates, out);
+            current.truncate(start_len);
         }
     }
 
+    let mut starts = conn.keys().copied().to_vec();
+    starts.sort_unstable();
+
     let mut result = String::new();
-    let mut visited = HashSet::new();
-    let mut start = HashSet::new();
-    recurse(&mut start, &conn, &mut result, &mut visited);
+    let mut current = String::with_capacity(conn.len() * 3);
+    let mut removed = HashSet::new();
+    for a in starts {
+        let mut candidates = conn.remove(a).unwrap(); // avoid duplicate checks by removing the node
+        candidates.retain(|&x| !removed.contains(x));
+
+        current.clear();
+        current.push_str(a);
+        recurse(&mut current, &conn, &candidates, &mut result);
+        removed.insert(a);
+    }
+
     result!(result);
 }
 use crate::utils::*;
